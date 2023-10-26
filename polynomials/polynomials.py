6fb2fc6ca252e8abcd39fcd8c50b225368c59ec5
Riyaaz Khan <riyaaz.khan22@imperial.ac.uk>
2023-10-26 15:34:50 +0100

    Commit1

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -50,3 +50,53 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+    def __sub___(self, other):
+        if isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a - b for a, b in zip(self.coefficients,
+                                                other.coefficients))
+            coefs += self.coefficients[common:] - other.coefficients[common:]
+            return Polynomial(coefs)
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] - other, +
+                               self.coefficients[1:]))
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        return (self - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            final_poly = [0] * (self.degree() + other.degree() + 1)
+            for i in range(len(self.coefficients)):
+                for j in range(len(other.coefficients)):
+                    final_poly[(i+j)] += (self.coefficients[i]
+                                          * other.coefficients[j])
+        elif isinstance(other, Number):
+            final_poly = (self * other)
+            final_poly = Polynomial(final_poly)
+            return final_poly
+
+    def __rmul__(self, other):
+        for i in range(len(self.coefficients)):
+            self.coefficent[i] = self.coefficient[i] * other
+        return self
+
+    def __pow__(self, other):
+        if isinstance(Polynomial, Number):
+            if Polynomial == 0:
+                final_poly = 0
+            else:
+                final_poly = 1
+                for i in range(other):
+                    final_poly *= self
+            return final_poly
+
+    def __call__(self, other):
+        final_poly = 0
+        if isinstance(Polynomial, Number):
+            for i, coefs in enumerate(self.coefficients):
+                final_poly += coefs * (other ** (self.degree() - i))
+        return final_poly
